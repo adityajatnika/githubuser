31014082039722c5168c0624e7f9a8ca6b69ff07
adityajatnika <adityaadjatnika2@gmail.com>
2022-08-09 00:59:54 +0700

    build security access token and url

--- app/src/main/java/com/example/githubuserapp/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/githubuserapp/data/remote/retrofit/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.example.githubuserapp.data.remote.retrofit
 
+import com.example.githubuserapp.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,13 +9,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object{
         fun getApiService(): ApiServices {
-            val loggingInterceptor =
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
+                .baseUrl(BuildConfig.API_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
--- app/src/main/java/com/example/githubuserapp/data/remote/retrofit/ApiServices.kt
+++ app/src/main/java/com/example/githubuserapp/data/remote/retrofit/ApiServices.kt
@@ -1,5 +1,6 @@
 package com.example.githubuserapp.data.remote.retrofit
 
+import com.example.githubuserapp.BuildConfig
 import com.example.githubuserapp.data.remote.response.DetailUserResponse
 import com.example.githubuserapp.data.remote.response.FindUserResponse
 import com.example.githubuserapp.data.remote.response.UserResponse
@@ -11,22 +12,22 @@ import retrofit2.http.Query
 
 interface ApiServices {
     @GET("users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token "+BuildConfig.API_TOKEN)
     fun getUsers(): Call<List<UserResponse>>
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token "+BuildConfig.API_TOKEN)
     fun findUsers(@Query("q") query: String): Call<FindUserResponse>
 
     @GET("users/{login}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token "+BuildConfig.API_TOKEN)
     fun getFollowers(@Path("login") login : String): Call<List<UserResponse>>
 
     @GET("users/{login}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token "+BuildConfig.API_TOKEN)
     fun getFollowing(@Path("login") login : String): Call<List<UserResponse>>
 
     @GET("users/{login}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token "+BuildConfig.API_TOKEN)
     fun getDetailUser(@Path("login") login : String): Call<DetailUserResponse>
 }
